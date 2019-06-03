e65b35fadee58d5c197417a5a02a6baefd64ca50
leylop <leidy.lopez@imexhs.com>
2019-06-03 16:09:18 -0500

    Add files via upload

---
+++ Main_mtx.java
@@ -0,0 +1,28 @@
+package MatrixMultiplicationFinal;
+
+public class Main_mtx {
+
+	public static void main(String[] args) {
+		
+		
+		
+		 
+	         
+	        int A[][] = { {1, 2, 3, 3}, 
+	                      {4, 5, 6, 5},
+	                      {1, 2, 7, 9},
+	                      {7, 8, 9, 6}}; 
+	   
+	        int B[][] = { {1, 2, 3, 3}, 
+	                      {4, 5, 6, 5}, 
+	                      {1, 2, 7, 9},
+	                      {7, 8, 9, 7} }; 
+	   
+	        MatrixMultiplication.matrixMultiplication(A, B,4,4, 4, 4, 4); 
+	    
+	
+		
+	}
+
+	
+}
